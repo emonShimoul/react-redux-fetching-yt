6515e3f439aa3372d3a1a5527b05051b5c11d278
emonShimoul <emonshimoul@gmail.com>
2023-01-07 01:05:17 +0600

    writing steps for workflows

--- src/App.js
+++ src/App.js
@@ -9,3 +9,13 @@ function App() {
 }
 
 export default App;
+
+
+// step1: install packages
+// step2: constant define
+// step3: asynchronous action creator
+// step4: reducer
+// step5: create store
+// step6: provide store
+// step7: use store in react app
+// step8: adding css
