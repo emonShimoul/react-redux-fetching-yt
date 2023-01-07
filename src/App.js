761fd22247174c31fba12d2a10720d19436ea818
emonShimoul <emonshimoul@gmail.com>
2023-01-07 16:05:43 +0600

    create todos component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>React-redux tutorial</h1>
+      <Todos></Todos>
     </div>
   );
 }
